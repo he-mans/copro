e662d70feb3ead9376d91f54c7077a260f1ade42
Himanshu Pandey <pandeyhimanshu710@gmail.com>
2018-11-13 10:12:05 +0530

    exception mention the server which raised them

--- server/main.py
+++ server/main.py
@@ -12,56 +12,66 @@ import server_scout
 
 def start_server_feed():
 	try:
+		1/0
 		server_feed.main()
 	except Exception as e:
+		print('server_feed raised exception :',end = ' ')
 		print(e)
 
 def start_server_login():
 	try:
 		server_login.main()
 	except Exception as e:
+		print('server_login raised exception :',end = ' ')
 		print(e)
 
 def start_server_create_account():
 	try:	
 		server_create_account.main()
 	except Exception as e:
+		print('server_create_account raised exception :',end = ' ')
 		print(e)
 
 def start_server_upload_feed():
 	try:
 		server_upload_feed.main()
 	except Exception as e:
+		print('server_upload_feed raised exception :',end = ' ')
 		print(e)
 
 def start_server_search():
 	try:
 		server_search.main()
 	except Exception as e:
+		print('server_search raised exception :',end = ' ')
 		print(e)
 
 def start_server_friend():
 	try:
 		server_friend.main()
 	except Exception as e:
+		print('server_friend raised exception :',end = ' ')
 		print(e)
 
 def start_server_change_settings():
 	try:
 		server_change_settings.main()
 	except Exception as e:
+		print('server_change_settings raised exception :',end = ' ')
 		print(e)
 
 def start_server_like():
 	try:
 		server_like.main()
 	except Exception as e:
+		print('server_like raised exception :',end = ' ')
 		print(e)
 
 def start_server_scout():
 	try:
 		server_scout.main()
 	except Exception as e:
+		print('server_scout raised exception :',end = ' ')
 		print(e)
 
 if __name__ == '__main__':
